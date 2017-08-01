d6678a839ad1753a9f31f87546d898bebb566998
tomcat <risc604@ms40.url.com.tw>
2017-08-01 14:24:02 +0800

    Android 7.0 Camera get image file path OK & test.

--- app/src/main/java/com/example/tomcat/nugatcamera/MainActivity.java
+++ app/src/main/java/com/example/tomcat/nugatcamera/MainActivity.java
@@ -5,22 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 //http://www.voidcn.com/blog/Hacker_ZhiDian/article/p-6580116.html
 
@@ -35,17 +40,29 @@ public class MainActivity extends AppCompatActivity
     private static final int CHOICE_ALBUM_REQUEST_CODE = 4;
     private static final int CORP_PHOTO_REQUEST_CODE = 5;
 
+    //private enum InnerRequestCode
+    //{
+    //    TAKE_PHOTO_PERMISSION_REQUEST_CODE,
+    //    WRITE_SDCARD_PERMISSION_REQUEST_CODE,
+    //
+    //    TAKE_PHOTO_REQUEST_CODE,
+    //    CHOICE_ALBUM_REQUEST_CODE,
+    //    CORP_PHOTO_REQUEST_CODE
+    //
+    //}
+
     private Uri photoUri = null;
     private Uri outputUri = null;
 
     ImageView   mImageView;
-    Button mButton;
+    Button      mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate(), savedInstanceState: " + savedInstanceState);
 
         initView();
         initControl();
@@ -53,19 +70,80 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        Log.i(TAG, "onRequestPermissionsResult(),  requestCode: " + requestCode +
+                ", permissions: " + Arrays.toString(permissions) +
+                ", grantResults: " + Arrays.toString(grantResults));
+
+        switch (requestCode)
+        {
+            case TAKE_PHOTO_PERMISSION_REQUEST_CODE:
+                if ((grantResults.length > 0) &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                {
+                    startCamera();
+                }
+                else
+                {
+                    Toast.makeText(this, "Camera permission decline.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case WRITE_SDCARD_PERMISSION_REQUEST_CODE:
+                if ((grantResults.length > 0) &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                {
+                }
+                else
+                {
+                    Toast.makeText(this, "SD card write permission decline.", Toast.LENGTH_SHORT).show();
+                }
+
+            default:
+                break;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
+        Log.i(TAG, "onActivityResult(),  requestCode: " + requestCode +
+                    ", resultCode: " + resultCode);
+        if (data!= null)
+            Log.i(TAG, "data: " + Arrays.toString(data.getStringArrayExtra(data.getDataString())));
         //super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode)
         {
-            case REQUEST_IMAGE_CAPTURE:
-                if (requestCode == RESULT_OK)
-                {
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmp = (Bitmap) extras.get("data");
-                    mImageView.setImageBitmap(imageBitmp);
-                }
+            case TAKE_PHOTO_REQUEST_CODE:
+                //if (requestCode == RESULT_OK)
+                //{
+                //    Bundle extras = data.getExtras();
+                //    Bitmap imageBitmp = (Bitmap) extras.get("data");
+                //    mImageView.setImageBitmap(imageBitmp);
+                //}
+                cropPhoto(photoUri);
                 break;
+
+            case CHOICE_ALBUM_REQUEST_CODE:
+                cropPhoto(data.getData());
+                break;
+
+            case CORP_PHOTO_REQUEST_CODE:
+                File file = new File(photoUri.getPath());
+                if (file.exists())
+                {
+                    Bitmap bitmap = BitmapFactory.decodeFile(photoUri.getPath());
+                    mImageView.setImageBitmap(bitmap);
+                }
+                else
+                {
+                    Toast.makeText(this, "NOT found image file.", Toast.LENGTH_SHORT).show();
+                }
 
             default:
                 break;
@@ -75,6 +153,7 @@ public class MainActivity extends AppCompatActivity
 
     public void ImgBtnOnClick(View view)
     {
+        Log.i(TAG, "ImgBtnOnClick(),  view: " + view.toString());
         if (view == mButton)
         {
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
@@ -97,12 +176,14 @@ public class MainActivity extends AppCompatActivity
 
     private void initView()
     {
+        Log.i(TAG, "initView() ... ");
         mImageView = (ImageView) findViewById(R.id.imageView);
         mButton = (Button) findViewById(R.id.btnCapture);
     }
 
     private void initControl()
     {
+        Log.i(TAG, "initControl() ... ");
         //----- permission check -----
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED)
@@ -115,6 +196,7 @@ public class MainActivity extends AppCompatActivity
 
     private void startCamera()
     {
+        Log.i(TAG, "startCamera() ... ");
         File file = new File(getExternalCacheDir(), "image.png");
         try
         {
@@ -129,9 +211,10 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
+        Log.i(TAG, "file: " + file.getPath());
         if (Build.VERSION.SDK_INT >= 24)
         {
-            photoUri = FileProvider.getUriForFile(this, "com.example.tomcat.nugatcamera", file);
+            photoUri = FileProvider.getUriForFile(this, "com.example.tomcat.nugatcamera.provider", file);
         }
         else
         {
@@ -145,6 +228,7 @@ public class MainActivity extends AppCompatActivity
 
     private void choiceAlbum()
     {
+        Log.i(TAG, "choiceAlbum() ...");
         Intent choiceAlbumIntent = new Intent(Intent.ACTION_GET_CONTENT);
         choiceAlbumIntent.setType("image/*");
         startActivityForResult(choiceAlbumIntent, CHOICE_ALBUM_REQUEST_CODE);
@@ -152,6 +236,7 @@ public class MainActivity extends AppCompatActivity
 
     private void cropPhoto(Uri inputUri)
     {
+        Log.i(TAG, "cropPhoto(), inputUri: " + inputUri.toString());
         Intent cropPhotoIntent = new Intent("com.android.camera.action.CROP");
         cropPhotoIntent.setDataAndType(inputUri, "image/*");
         cropPhotoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
